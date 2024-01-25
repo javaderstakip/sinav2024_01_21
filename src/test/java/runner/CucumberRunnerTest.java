4c578cfd75f8e858bdc6b5a30dc28f04b1514904
Asus <mehmetjavaderstakip@gmail.com>
2024-01-25 17:44:51 +0300

    saat1744

--- src/test/java/runner/CucumberRunnerTest.java
+++ src/test/java/runner/CucumberRunnerTest.java
@@ -3,7 +3,7 @@ package runner;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(features = {"src/test/resources/features"},
+@CucumberOptions(features = {"src/test/resources/features/saucedemo5.feature"},
     glue = {"stepDefinitions"},
     tags = "")
 public class CucumberRunnerTest extends AbstractTestNGCucumberTests {
