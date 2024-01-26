440e263beefce116564940815764bf9d8a7cec83
javaderstakip <mehmetjavaderstakip@gmail.com>
2024-01-26 23:00:38 +0300

    20240126 calisir

--- src/test/java/runner/CucumberRunnerTest.java
+++ src/test/java/runner/CucumberRunnerTest.java
@@ -3,9 +3,9 @@ package runner;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(features = {"src/test/resources/features/saucedemo5.feature"},
+@CucumberOptions(features = {"src/test/resources/features"},
     glue = {"stepDefinitions"},
-    tags = "")
+    tags = "@milestone_6")
 public class CucumberRunnerTest extends AbstractTestNGCucumberTests {
     //"@milestone_6 and not @milestone_6_6" taginde not @milestone_6_6 CALISIYOR CUNKU 6_6 DIYE BIR TAG YOK 6_5 VAR AMA...
 }
